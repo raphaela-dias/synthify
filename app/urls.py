a4a54d659a72d324c138f4e277e0ecb2b2f629dc
RLMD <raphaelalmd@gmail.com>
2024-08-21 12:17:36 -0300

    Conectadas as imagens/css da loginPage.html

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.views import index
+from app.views import index, loginPage
 
 urlpatterns = [
-        path('', index)
+        path('', index),
+        path('loginPage.html', loginPage)
 ]
